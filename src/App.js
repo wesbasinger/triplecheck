f12674f19125106a112b900169cc075e2213a454
wesbasinger <wbasinger@villagetechschools.org>
2016-09-18 07:34:21 -0500

    Put all the components in their places.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,86 @@ var React = require('react');
 
 var triples = require('./triples');
 
+var Points = React.createClass({
+  render: function() {
+    return(
+      <div>Points: {this.props.score}</div>
+    )
+  }
+});
+
+var ADisplay = React.createClass({
+  render: function() {
+    return(
+      <span>A</span>
+    )
+  }
+});
+
+var BDisplay = React.createClass({
+  render: function() {
+    return(
+      <span>B</span>
+    )
+  }
+});
+
+var CDisplay = React.createClass({
+  render: function() {
+    return(
+      <span>C</span>
+    )
+  }
+});
+
+var Triple = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <ADisplay />
+        <BDisplay />
+        <CDisplay />
+      </div>
+    )
+  }
+});
+
+var InputBar = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <input type="number" required="yes" value="" onChange={this.changeHandler} />
+        <button onClick={this.handleClick}>Check</button>
+      </div>
+    )
+  }
+})
+
 var App = React.createClass({
 
   getInitialState: function() {
     return {
       score: 0,
-      triples: []
+      triples: [],
+      curr: ""
     }
   },
 
-  componentDidMount: function() {
+  componentWillMount: function() {
     this.setState({triples: triples});
   },
 
+  componentDidMount: function() {
+    this.setState({curr: this.state.triples.shift()})
+  },
 
   render: function() {
-    return (
-      <div>Hello World!</div>
+    return(
+      <div>
+        <Points score={this.state.score}/>
+        <Triple />
+        <InputBar />
+      </div>
     )
   }
 })
