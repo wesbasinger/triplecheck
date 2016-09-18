1e517f7afed0e2b64cdf0d2286c3d96f149d2547
wesbasinger <wbasinger@villagetechschools.org>
2016-09-17 21:15:33 -0500

    Loading in the triples on componentDidMount.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 var React = require('react');
 
+var triples = require('./triples');
+
 var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      score: 0,
+      triples: []
+    }
+  },
+
+  componentDidMount: function() {
+    this.setState({triples: triples});
+  },
+
+
   render: function() {
     return (
       <div>Hello World!</div>
