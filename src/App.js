574aa59d1b4ab1d879a5168a5b51a4b93160afff
wesbasinger <wbasinger@villagetechschools.org>
2016-09-18 20:56:17 -0500

    Passing a mask index down to the individual variable components.

--- src/App.js
+++ src/App.js
@@ -12,35 +12,49 @@ var Points = React.createClass({
 
 var ADisplay = React.createClass({
   render: function() {
-    return(
-      <span>A</span>
-    )
+    if (this.props.maskIndex === 0) {
+      return <span>*</span>
+    } else {
+      return(
+        <span>{this.props.a}</span>
+      )
+    }
   }
 });
 
 var BDisplay = React.createClass({
   render: function() {
-    return(
-      <span>B</span>
-    )
+    if (this.props.maskIndex === 1) {
+      return <span>*</span>
+    } else {
+      return(
+        <span>{this.props.b}</span>
+      )
+    }
   }
 });
 
 var CDisplay = React.createClass({
   render: function() {
-    return(
-      <span>C</span>
-    )
+    if (this.props.maskIndex === 2) {
+      return <span>*</span>
+    } else {
+      return(
+        <span>{this.props.c}</span>
+      )
+    }
   }
 });
 
 var Triple = React.createClass({
+
   render: function() {
+
     return(
       <div>
-        <ADisplay />
-        <BDisplay />
-        <CDisplay />
+        <ADisplay a={this.props.curr[0]} maskIndex={this.props.maskIndex}/>
+        <BDisplay b={this.props.curr[1]} maskIndex={this.props.maskIndex}/>
+        <CDisplay c={this.props.curr[2]} maskIndex={this.props.maskIndex}/>
       </div>
     )
   }
@@ -75,11 +89,15 @@ var App = React.createClass({
     this.setState({curr: this.state.triples.shift()})
   },
 
+  rand: function() {
+    return Math.floor(Math.random() * (2));
+  },
+
   render: function() {
     return(
       <div>
         <Points score={this.state.score}/>
-        <Triple />
+        <Triple curr={this.state.curr} maskIndex={this.rand()}/>
         <InputBar />
       </div>
     )
